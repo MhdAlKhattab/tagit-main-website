904fe14dc1b0cdec614e59ad42bd76b936865a71
MhdAlKhattab <mohammadalkhatab123@gmail.com>
2024-01-13 16:54:14 +0300

    Re-upload

---
+++ resources/views/includes/main.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="">
+@include('includes.header')
+
+<body>
+<div class="page-wrapper">
+    <!-- Preloader -->
+    <div class="preloader"></div>
+    <!-- Main Header-->
+    @yield('content')
+
+</div><!-- End Page Wrapper -->
+
+</body>
+@include('includes.footer')
+</html>
